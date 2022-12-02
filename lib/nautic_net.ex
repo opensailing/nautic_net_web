cc28b5c37fbfb8d3e747d8597bfb1e09f9597973
Rockwell Schrock <rockwell@schrock.me>
2022-12-02 11:25:59 -0500

    Fix filenames

---
+++ lib/nautic_net.ex
@@ -0,0 +1,9 @@
+defmodule NauticNet do
+  @moduledoc """
+  NauticNet keeps the contexts that define your domain
+  and business logic.
+
+  Contexts are also responsible for managing your data, regardless
+  if it comes from the database, an external API or others.
+  """
+end
