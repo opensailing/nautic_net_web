3813c09b041e6050ca49f481a689a96eb65ea753
Rockwell Schrock <rockwell@schrock.me>
2023-01-12 14:30:12 -0500

    Get boat_identifier from decoded DataSet

--- lib/nautic_net_web/controllers/api/data_set_controller.ex
+++ lib/nautic_net_web/controllers/api/data_set_controller.ex
@@ -5,20 +5,14 @@ defmodule NauticNetWeb.API.DataSetController do
   alias NauticNet.Protobuf.DataSet
   alias NauticNet.Racing
 
-  def create(
-        conn,
-        %{
-          "boat_identifier" => identifier,
-          "proto_base64" => proto_base64
-        }
-      ) do
-    boat = Racing.get_or_create_boat_by_identifier(identifier)
-
+  def create(conn, %{"proto_base64" => proto_base64}) do
     data_set =
       case Base.decode64(proto_base64) do
         {:ok, binary} -> DataSet.decode(binary)
         :error -> raise "oh no"
       end
+
+    boat = Racing.get_or_create_boat_by_identifier(data_set.boat_identifier)
 
     DataIngest.insert_samples!(boat, data_set)
 
