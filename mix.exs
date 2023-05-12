a04c1ca71a556cdc8c8e1e394a5fdb6489dcc453
Christian Tovar <tovarchristian21@gmail.com>
2023-05-12 11:46:47 -0500

    Add :test env for :dialyxir
    Co-authored-by: Paulo Valente <16843419+polvalente@users.noreply.github.com>

--- mix.exs
+++ mix.exs
@@ -68,7 +68,7 @@ defmodule NauticNet.MixProject do
 
       # Development
       {:live_inspect, "~> 0.2"},
-      {:dialyxir, "~> 1.3", only: [:dev], runtime: false},
+      {:dialyxir, "~> 1.3", only: [:dev, :test], runtime: false},
 
       # NauticNet
       {:nautic_net_protobuf, github: "DockYard/nautic_net_protobuf"},
