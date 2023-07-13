fa57d8ba794b7b7eafd34cacfe4334e0501dd448
Rockwell Schrock <rockwell@schrock.me>
2023-07-13 14:32:38 -0400

    Auto refresh boat page every 10 seconds

--- lib/nautic_net_web/live/boats_live.ex
+++ lib/nautic_net_web/live/boats_live.ex
@@ -5,11 +5,21 @@ defmodule NauticNetWeb.BoatsLive do
   alias NauticNet.Racing.BoatStats
 
   def mount(_, _, socket) do
-    boats_stats = Racing.list_boats_stats()
-
-    socket = assign(socket, boats_stats: boats_stats)
+    socket = load(socket, :boat_stats)
+    Process.send_after(self(), :refresh, :timer.seconds(10))
 
     {:ok, socket}
+  end
+
+  def handle_info(:refresh, socket) do
+    socket = load(socket, :boat_stats)
+    Process.send_after(self(), :refresh, :timer.seconds(10))
+
+    {:noreply, socket}
+  end
+
+  defp load(socket, :boat_stats) do
+    assign(socket, boats_stats: Racing.list_boats_stats())
   end
 
   defp status_label(%{boat_stats: %BoatStats{} = boat_stats} = assigns) do
